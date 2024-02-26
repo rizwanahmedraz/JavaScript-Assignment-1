28f85a258b0555ddadea8d8108b6144995f8c25a
Rizwanahmed <rizwanahmed6647@gmail.com>
2024-02-26 23:11:50 +0500

    added index and app.s

---
+++ app.js
@@ -0,0 +1,84 @@
+   //Chapter1      
+
+ //  Q1   
+
+//alert("Error! Please enter your password");   
+
+// Q2   
+
+//alert("Welcome to JS Land...\n Happy Coding");
+
+// Q3   
+
+//alert("Welcome to JS Land...");
+
+//alert("Happy Coding");
+
+//  Q4
+//console.log("Hello... I can run JS throuhg my wen browser'console");
+
+//Chapter 2
+
+//Q1
+//  var username;
+//  var myName=("John Doe");
+  
+//  alert(myName);
+
+//  var age=("15 years old");
+
+//  alert(age);
+
+//  var education=("Certified Mobile Application Development");
+
+//  alert(education);
+
+// Q2
+
+//  var pizza=(" PIZZA\n PIZZ \n PIZ \n PI \n P");
+
+//  alert(pizza)
+
+//  Q3
+//  var Email=("My email address is rizwanahmed6647@gmail.com");
+
+//  alert(Email);
+
+// Q4
+//  var book
+//  var bookName="A smart way to learn JavaScript";
+//  alert("I am trying to learn from the Book A smarter\n way to learn JavaScript");
+
+//  Q5
+
+//  document.write("Yah! I can write HTML content through JavaScript");
+
+//  Q6
+
+//  var taj=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+//  alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+//  Chapter 3
+
+//  Q1
+
+//  var age=("I am 15 years Old");
+//  alert(age)
+
+
+//  Q2
+
+//  var visitor;
+//  visitor="you have visited 14 times"
+//  alert(visitor);
+
+//  Q3
+
+//  var birthYear;
+//  document.write("My birth year is 1995\n <br>  Data type of my decleard variable is number");
+
+//  Q4
+
+//  var visitorName;
+//  document.write("<b>Rizwan Ahmed</b> ordered <b> 5 T'Shirt</b>(s) on Raz Clothing Store");
